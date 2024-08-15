161df2ff046f40e418da726c78018327b04ee509
SharaSison <sisonshara01@gmail.com>
2024-08-15 18:31:08 +0800

    main

---
+++ library/library_extensions/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Library Extensions',
+    'version': '1.0',
+    'depends': ['library'],
+    'data': [
+        'views/library_book_views.xml',
+        'views/library_book_category_views.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
